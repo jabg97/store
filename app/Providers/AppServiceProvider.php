7d812da4ec4040f22a4c530d521c0f3e57c162f6
jaiver1 <32825826+jaiver1@users.noreply.github.com>
2020-04-19 16:13:34 -0500

    Fix HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
